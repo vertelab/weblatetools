292d45749d60924e4e31c703f1a219909270ad4f
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-08-13 09:57:38 +0200

    T/5088 Create vertel_svpo.sh

---
+++ vertel_svpo/vertel_svpo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# 2025-08-12
+# Vertel internal
+# List all modules where sv_SE.po file is missing
+# Create an odootool shortcut to paste in the Terminal.
+
+# Run this file:
+# chmod +x vertel_svpo.sh
+# ./vertel_svpo.sh
+
+# Sökvägens mönster
+BASE_PATH="/usr/share/odoo-*/*"
+
+echo "Vertel: Letar efter kataloger där 'sv.po' saknas..."
+
+# Leta igenom alla matchande kataloger
+for dir in $BASE_PATH; do
+    # Kontrollera om det är en katalog
+    if [ -d "$dir" ]; then
+       if [ ! -f "$dir/i18n/sv.po" ] && [ ! -f "$dir/i18n/sv_SE.po" ]; then
+            module_name=$(basename "$dir")
+            echo "Saknas: $dir"
+            echo "odoolangexport -m $module_name -d database -l sv_SE"
+            echo " "
+        fi
+    fi
+done
