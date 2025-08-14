789b6f7e643213c3a0f121199aab2290ba39a163
Anders Wallenquist <anders.wallenquist@vertel.se>
2025-08-14 15:25:14 +0000

    deleted:    create_po.sh

--- local_po/create_po.sh
+++
@@ -1,7 +0,0 @@
-
-
-
- 
-odoo --config ${ODOO_SERVER_CONF} --database ${ODOOREPO} --init ${ODOOMODULES} --stop-after-init
-
-"odoo -c /etc/odoo/odoo.conf --modules='${MODULES}' -d ${DATABASES} ${LCMD} --stop-after-init --i18n-export='${FILE}'"
